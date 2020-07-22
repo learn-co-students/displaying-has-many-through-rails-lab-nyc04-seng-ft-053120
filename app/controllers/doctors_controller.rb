abcf79f225ba1813c19e748edc4ef86181845e77
kceron <karemiceron@gmail.com>
2020-07-22 14:27:05 -0400

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,21 @@
+class DoctorsController < ApplicationController
+
+    def index 
+        @doctors = Doctor.all
+
+    end
+
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def new
+
+    end
+
+    def create 
+
+    end
+
+
+end
